2d6fb33e0948f288904419f5e293c8d2ef3f1da3
niczyp <zypc0001@algonquinlive.com>
2025-04-03 17:43:37 -0400

    completed PublicSchool.java

--- Desktop/CP SEMESTER 4/Enterprise Programming CST8277/Assignments/Group Assignment/REST-ACMEMedical-Skeleton/REST-ACMEMedical-Skeleton/src/main/java/acmemedical/entity/PublicSchool.java
+++ Desktop/CP SEMESTER 4/Enterprise Programming CST8277/Assignments/Group Assignment/REST-ACMEMedical-Skeleton/REST-ACMEMedical-Skeleton/src/main/java/acmemedical/entity/PublicSchool.java
@@ -9,8 +9,13 @@ package acmemedical.entity;
 
 import java.io.Serializable;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
 //TODO PUSC01 - Add missing annotations, please see Week 9 slides page 15.  Value 1 is public and value 0 is private.
 //TODO PUSC02 - Is a JSON annotation needed here?
+@Entity
+@DiscriminatorValue("1")
 public class PublicSchool extends MedicalSchool implements Serializable {
 	private static final long serialVersionUID = 1L;
 
